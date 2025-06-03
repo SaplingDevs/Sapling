62ae798cd76cf002b85a1e3242cfe1067c4952e2
AlecsDeveloper <alecsdelvalledetorres@gmail.com>
2025-06-03 13:38:24 -0500

    Bundler regolith filter

--- source/typescript/classes/Utils.ts
+++ source/typescript/classes/Utils.ts
@@ -21,9 +21,9 @@ export class Utils {
 
   static getBlockFromBase(base: Block, coords: number[]): Block {
     let locB = {
-        x: base.location.x + coords[0],
-        y: base.location.y + coords[1],
-        z: base.location.z + coords[2]
+      x: base.location.x + coords[0],
+      y: base.location.y + coords[1],
+      z: base.location.z + coords[2]
     };
     
     return base.dimension.getBlock(locB);
@@ -45,37 +45,3 @@ export class Utils {
     return [ ...overworld, ...nether, ...the_end ];
   }
 }
-
-// export default class Utils2 {
-    
-
-//     static getBlockFromBase(base, coords) {
-//         let locB = {
-//             x: base.location.x + coords[0],
-//             y: base.location.y + coords[1],
-//             z: base.location.z + coords[2]
-//         };
-        
-//         return base.dimension.getBlock(locB);
-//     }
-
-//     static getDirection(angle) {
-//         if(angle < 0) angle = 360 - Math.abs(angle);
-//         if((225 < angle) && (angle <= 315)) return 'east (Towards positive X)';
-//         else if((45 <= angle) && (angle < 135)) return 'west (Towards negative X)';
-//         else if ((135 < angle) && (angle <= 225)) return 'north (Towards negative Z)';
-//         else return 'south (Towards positive Z)';
-//     }
-
-//     static getAllEntities(options) {
-//         const overworld = world.getDimension('overworld').getEntities(options);
-//         const nether = world.getDimension('nether').getEntities(options);
-//         const the_end = world.getDimension('the_end').getEntities(options);
-      
-//         return [ ...overworld, ...nether, ...the_end ];
-//     }
-
-//     static privateMessage(sender, txt) {
-//         system.run(() => sender.runCommand(`tellraw @s { "rawtext": [ { "text": "${txt}" } ] }`));
-//     }
-// }
